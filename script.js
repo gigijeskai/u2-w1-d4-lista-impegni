a871629fd0b65096adc96fc73f62d8c27fb92375
Luigi Indaco <luigindaco@gmail.com>
2022-09-29 16:58:12 +0200

    consegna

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-function select(){
-   let item = document.getElementsByClassName('item')
+let inserisciNota = document.getElementById('input');
+let inputButton = document.getElementById('input-button');
+let listaNote = document.getElementById('lista-note');
+
+inputButton.addEventListener('click',aggiungiNota)
+
+function aggiungiNota(){
+if (inserisciNota.value === ''){
+   alert('devi scrivere qualcosa')
+}
+let li = document.createElement('li');
+
+li.appendChild(document.createTextNode(inserisciNota.value))
+
+// let collegamento = document.createElement('collegamento')
+
+li.appendChild(collegamento)
+
+listaNote.appendChild(li)
+
+inserisciNota.value = ''
 
 }
